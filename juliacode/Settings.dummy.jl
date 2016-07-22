65b9ba7bda630308bba480cb6b677c8ac5884dc9
Thomas Willwacher <twillw@Thomass-MacBook-Pro-2.local>
2016-07-22 09:55:22 +0200

    added settings file (blueprint)

---
+++ juliacode/Settings.dummy.jl
@@ -0,0 +1,11 @@
+###################################################################################################
+# In this file machine dependent settings are defined
+# Rename to Settings.jl and edit the entries according to the local setup 
+###################################################################################################
+
+# The base directory where data files are stored
+DATA_DIR="../gholdata"
+
+# The directory where matrix files are queued for external rank computation
+EXCHANGE_DIR_RANK="../exchange/rank"
+
