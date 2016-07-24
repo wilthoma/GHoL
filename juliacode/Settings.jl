2dac3df33a3834008a4824982813c579354213d1
Thomas Willwacher <twillw@Thomass-MacBook-Pro-2.local>
2016-07-24 21:37:29 +0200

    wrapper code

---
+++ juliacode/Settings.jl
@@ -0,0 +1,10 @@
+###################################################################################################
+# In this file machine dependent settings are defined
+###################################################################################################
+
+# The base directory where data files are stored
+DATA_DIR="../../gholdata"
+
+# The directory where matrix files are queued for external rank computation
+EXCHANGE_DIR_RANK="../../exchange/rank"
+
